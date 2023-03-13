5780de83864cbc27b98269cc3e774058831543bb
Jetro De Leon <j08mark@hotmail.com>
2023-03-13 11:15:53 -0500

    Project is now a C++ program, with a player character and associated files

---
+++ Source/BlockHeadCppW2023/BlockHeadCppW2023.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BlockHeadCppW2023.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BlockHeadCppW2023, "BlockHeadCppW2023" );
