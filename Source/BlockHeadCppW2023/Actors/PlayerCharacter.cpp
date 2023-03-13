cae0460f882f8d54aca0bca8771b840548b23676
Jetro De Leon <j08mark@hotmail.com>
2023-03-13 12:05:22 -0500

    Added log statements in the Player and GameMode C++ and Blueprint files

--- Source/BlockHeadCppW2023/Actors/PlayerCharacter.cpp
+++ Source/BlockHeadCppW2023/Actors/PlayerCharacter.cpp
@@ -8,14 +8,23 @@ APlayerCharacter::APlayerCharacter()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	UE_LOG(LogTemp, Warning, TEXT("Hello from C++ Player Character Constructor"));
 }
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
+	int32 Answer = 42;
+	float Pi = 3.14;
+	FString Msg{ "This is a message" };
+
 	Super::BeginPlay();
-	
+	UE_LOG(LogTemp, Warning, TEXT("Hello from C++ Player Character BeginPlay"));
+	UE_LOG(LogTemp, Warning, TEXT("Hello Logging int: %d"), Answer);
+	UE_LOG(LogTemp, Warning, TEXT("Hello Logging float: %f"), Pi);
+	UE_LOG(LogTemp, Warning, TEXT("Hello Logging FString: %s"), *Msg);
+	GEngine->AddOnScreenDebugMessage(0, 5.0f, FColor::Red, TEXT("On Screen Hello from C++ Player Character BeginPlay"));
+
 }
 
 // Called every frame
